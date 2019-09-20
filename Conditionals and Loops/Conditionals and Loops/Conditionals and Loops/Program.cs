24338a0357f9e15090ccc5500dce98888fce1083
Victor4X <viggo00@hotmail.com>
2019-09-20 17:33:28 +0200

    Started the project with a simple switch statement test, and a debug QOL trick

---
+++ Conditionals and Loops/Conditionals and Loops/Conditionals and Loops/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals_and_Loops
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int num = 3;
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("one");
+                    break;
+
+                case 2:
+                    Console.WriteLine("two");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Three");
+                    break;
+            }
+
+            // Debugging tool to prevent instantly closing the console upon program completion
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.WriteLine("Press any key to close...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
